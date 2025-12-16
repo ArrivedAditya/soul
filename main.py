479e789a2ec72c1e6c8223e5c11c883ac9f4d2d5
Aaditya Anand <aa3014429@gmail.com>
2025-12-16 13:08:08 +0530

    working on pototype

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+class base():
+    '''Universal base model class for soul'''
+    def __init__(self):
+        pass
+    def recieve(self):
+        pass
+    def process(self):
+        pass
+    def interact(self):
+        pass
+
+class memory():
+    '''Memory class which supports the base model remerber and its interacts withs'''
+    def __init__(self):
+        pass
+    def reflex(self):
+        pass
+    def shortTerm(self):
+        pass
+    def longTerm(self):
+        pass
+
 def main():
-    print("Soul ver 0.0.0")
+    print("Soul ver 0.0.1 pre alpha")
 
 
 if __name__ == "__main__":
